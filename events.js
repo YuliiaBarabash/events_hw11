e9f65cd525536b83795ae1497726c4f5bf358c22
Yuliia Barabash <yuliiabarabash@MacBook-Pro-Yuliia.local>
2023-05-29 19:29:09 +0300

    const not let

--- events.js
+++ events.js
@@ -1,14 +1,14 @@
 function addDiv() {
-    let newDiv = document.createElement('div');
-    newDiv.className = 'block';
-    newDiv.textContent = 'Hello World!';
-    document.body.append(newDiv);
-    }
-    
- function noDiv() {
-   let noDiv = document.querySelector('div');
-   document.body.removeChild(noDiv);
- }
+  const newDiv = document.createElement("div");
+  newDiv.className = "block";
+  newDiv.textContent = "Hello World!";
+  document.body.append(newDiv);
+}
 
-document.querySelector('.test-input').addEventListener('mouseover', addDiv);
-document.querySelector('.test-input').addEventListener('mouseout', noDiv);
+function noDiv() {
+  const noDiv = document.querySelector("div");
+  document.body.removeChild(noDiv);
+}
+
+document.querySelector(".test-input").addEventListener("mouseover", addDiv);
+document.querySelector(".test-input").addEventListener("mouseout", noDiv);
